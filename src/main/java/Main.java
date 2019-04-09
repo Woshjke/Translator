b0516517bf47a5d8a278c0978347aa1c0b4ae208
zvash <Zvashkevich1999>
2019-04-10 01:51:39 +0300

    Добавлен маппинг json-а в объект

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 package main.java;
 
-import main.java.Translator.*;
+import main.java.translator.*;
 import java.io.IOException;
 
 public class Main {
--- src/main/java/translator/Translator.java
+++ src/main/java/translator/Translator.java
@@ -1,4 +1,7 @@
-package main.java.Translator;
+package main.java.translator;
+
+import main.java.jsonReaders.TranslatorJsonReader;
+import main.java.response.TranslatorResponse;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.io.DataOutputStream;
@@ -6,27 +9,32 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.List;
 
 public class Translator {
-
-    private static final String URL = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190409T183919Z.e60acd8daff6f806.f569e10ca83dbbcd54d77d8a953032818eca7282";
+    //Ссылка на API Яндекс Переводчика
+    private static final String URL = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=";
+    private static final String key = "trnsl.1.1.20190409T183919Z.e60acd8daff6f806.f569e10ca83dbbcd54d77d8a953032818eca7282";
 
     public static String translate(String lang, String input) throws IOException {
 
-        URL urlObj = new URL(URL);
+        URL urlObj = new URL(URL + key);
         HttpsURLConnection connection = (HttpsURLConnection) urlObj.openConnection();
         connection.setRequestMethod("POST");
         connection.setDoOutput(true);
         DataOutputStream dataOutputStream = new DataOutputStream(connection.getOutputStream());
         dataOutputStream.writeBytes("text=" + URLEncoder.encode(input, "UTF-8") + "&lang=" + lang);
+
         //Получаем ответ от сервера в виде json-файла
         InputStream response = connection.getInputStream();
         String json = new java.util.Scanner(response).nextLine();
-        //Выводим ответ от сервера на экран
-        System.out.println("Ответ от сервера: " + json);
+
+        //Получаем объект TranslatorResponse из json-a и выводим инормацию об объекте на экран
+        TranslatorJsonReader reader = new TranslatorJsonReader();
+        TranslatorResponse translatorResponse = reader.getTranslatorResponse(json);
+        System.out.println("Ответ от сервера: " + translatorResponse);
+
         //Получаем из json-a переведенную строку.
-        // TODO: 10.04.2019 нужно написать класс TranslatorResponse, объекты которого будут хранить в себе ответ
-        //  от сервера (код операции, язык перевода и переведенный текст)
         int start = json.indexOf("[");
         int end = json.indexOf("]");
         String translated = json.substring(start + 2, end - 1);
